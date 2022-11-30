9543f4764d8eec0b1fe13bd193cf91ce6b002328
nkzwchen <nkzwchen@outlook.com>
2022-12-01 00:14:32 +0800

    add L-B equation demo

---
+++ equation/liouville_bratu_equation.py
@@ -0,0 +1,75 @@
+import mindspore as md
+from sympy import *
+from mindspore import nn, ops
+from mindspore import Tensor, Parameter
+from mindspore import dtype as mstype
+import equation
+
+class LiouvilleBratuEquationLossNet(nn.Cell):
+    """NetLoss"""
+    def __init__(self, net, Lambda):
+        super(LiouvilleBratuEquationLossNet, self).__init__()
+        self.matmul = nn.MatMul()
+        self.grad = ops.composite.GradOperation()
+        self.sum = ops.ReduceSum()
+        self.mean = ops.ReduceMean()
+        self.net = net
+        self.Lambda = Lambda
+        self.exp = md.ops.Exp()
+
+    def get_variable(self, InSet, BdSet, g_fun_val):
+        """Get Parameters for NetLoss"""
+        assert BdSet.has_neumann_boundary == False, "LiouvilleBratu equation only support dirichlet boundary"
+        self.InSet_size = InSet.size
+        self.InSet_dim = InSet.dim
+        self.InSet_area = InSet.area
+        
+        InSet_g = g_fun_val[0]
+        InSet_gx = g_fun_val[1]
+        self.InSet_g = Parameter(
+            Tensor(InSet_g, mstype.float32), name="InSet_g", requires_grad=False)
+        self.InSet_l = Parameter(
+            Tensor(InSet.l, mstype.float32), name="InSet_l", requires_grad=False)
+        self.InSet_gx = Parameter(
+            Tensor(InSet_gx, mstype.float32), name="InSet_gx", requires_grad=False)
+        self.InSet_lx = Parameter(
+            Tensor(InSet.lx, mstype.float32), name="InSet_lx", requires_grad=False)
+
+        self.InSet_c = Parameter(
+            Tensor(InSet.c, mstype.float32), name="InSet_c", requires_grad=False)
+
+    def construct(self, InSet_x):
+        """forward"""
+        InSet_f = self.net(InSet_x)
+        InSet_fx = self.grad(self.net)(InSet_x)
+        InSet_u = self.InSet_g + self.InSet_l * InSet_f
+        InSet_ux = self.InSet_gx + self.InSet_lx * InSet_f + self.InSet_l * InSet_fx
+
+        InSet_loss = 0.5 * self.InSet_area * self.sum(self.mean((InSet_ux ** 2), 0)) - \
+               self.Lambda * self.InSet_area * self.mean(self.exp(InSet_u)) + \
+               self.InSet_area * self.mean(self.InSet_c * InSet_u)
+               
+        return InSet_loss
+               
+
+
+
+class LiouvilleBratuEquation():
+     def __init__(self, Lambda, u, x):  
+        self.Lambda = Lambda
+        x1 = x[0]
+        x2 = x[1] 
+        
+        r = diff(u, x1, 2) + diff(u, x2, 2) + self.Lambda * exp(u)
+        rhs_function = equation.Function(input=[x1, x2], output=[r])
+        u_function = equation.Function(input=[x1, x2], output=[u])
+        self.right_handle = self._get_right_handle(rhs_function)
+        self.ground_handle = self._get_ground_handle(u_function)
+          
+     def _get_right_handle(self, rhs_function):
+          return rhs_function
+
+     def _get_ground_handle(self, u_function):
+          return u_function
+
+
---
+++ liouville_bratu_equation.py
@@ -0,0 +1,105 @@
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Run PFNN"""
+import argparse
+import os
+import numpy as np
+from mindspore import Tensor
+from mindspore import dtype as mstype
+from mindspore import nn
+from src import pfnnmodel
+from data import gendata
+from train import PfnnSolver
+from sympy import symbols
+from equation import LiouvilleBratuEquation, LiouvilleBratuEquationLossNet
+LAMBDA = 0.5
+def get_equation():
+    x1, x2 = symbols("x1 x2")
+    u = ((x1 ** 3 - 3 * x1) * (x2 ** 3 - 3 * x2) - 3)
+    
+    return LiouvilleBratuEquation(LAMBDA , u, [x1, x2])
+
+def ArgParse():
+    """Get Args"""
+    parser = argparse.ArgumentParser(
+        description="Penalty-Free Neural Network Method")
+    parser.add_argument("--bound", type=float, default=[-1.0, 1.0, -1.0, 1.0],
+                        help="lower and upper bound of the domain")
+    parser.add_argument("--inset_nx", type=int, default=[60, 60],
+                        help="size of the inner set")
+    parser.add_argument("--bdset_nx", type=int, default=[60, 60],
+                        help="size of the boundary set")
+    parser.add_argument("--teset_nx", type=int, default=[101, 101],
+                        help="size of the test set")
+    parser.add_argument("--g_epochs", type=int, default=6000,
+                        help="number of epochs to train neural network g")
+    parser.add_argument("--f_epochs", type=int, default=6000,
+                        help="number of epochs to train neural network f")
+    parser.add_argument("--g_lr", type=float, default=0.01,
+                        help="learning rate to train neural network g")
+    parser.add_argument("--f_lr", type=float, default=0.01,
+                        help="learning rate to train neural network f")
+    parser.add_argument("--device", type=str, default="gpu", choices=["cpu", "gpu"],
+                        help="use cpu or gpu to train")
+
+    parser.add_argument("--path", type=str, default="./optimal_state/",
+                        help="the basic folder of g_path and f_path")
+    parser.add_argument("--g_path", type=str, default="optimal_state_g_pfnn.ckpt",
+                        help="the path that will put checkpoint of netg")
+    parser.add_argument("--f_path", type=str, default="optimal_state_f_pfnn.ckpt",
+                        help="the path that will put checkpoint of netf")
+    _args = parser.parse_args()
+    return _args
+
+def neumann_filter(x):
+    return (np.zeros_like(x[..., 0]) > 1e-8)
+
+def dirichlet_filter(x):
+    return (np.zeros_like(x[..., 0]) < 1e-8)
+
+if __name__ == "__main__":
+
+    args = ArgParse()
+    equation = get_equation()
+
+    errors = None
+
+    if not os.path.exists(args.path):
+        os.mkdir(args.path)
+
+    args.g_path = args.path + args.g_path
+    args.f_path = args.path + args.f_path
+        
+    lenfac = pfnnmodel.LenFac(
+        Tensor(args.bound, mstype.float32).reshape(2, 2), 1, dirichlet_sides=['x_inf', 'x_sup', 'y_inf', 'y_sup'])
+    InSet, BdSet, TeSet = gendata.GenerateSet(args, lenfac, equation, dirichlet_filter, neumann_filter)   
+   
+    netg = pfnnmodel.NetG()
+    netf = pfnnmodel.NetF()
+    loss_net_f = LiouvilleBratuEquationLossNet(netf, LAMBDA)
+    optimg = nn.Adam(netg.trainable_params(), learning_rate=args.g_lr)
+    optimf = nn.Adam(netf.trainable_params(), learning_rate=args.f_lr)
+    solver = PfnnSolver(args = args,
+                        InSet = InSet,
+                        BdSet = BdSet,
+                        TeSet = TeSet,
+                        net_f = netf,
+                        net_g = netg,
+                        loss_net_f = loss_net_f,
+                        lenfac = lenfac,
+                        optim_f = optimf,
+                        optim_g = optimg)
+    solver.solve()
+
